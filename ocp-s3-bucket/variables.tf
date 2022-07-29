159b5230f0132e77e9b860c55573a9208e730dd5
Mathieu Grzybek <mathieu.grzybek@justice.gouv.fr>
2022-07-29 17:17:14 +0200

    [terraform] use external modules

--- ocp-s3-bucket/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "namespace" {
-  type        = string
-  description = "The namespace used to install the bucket's config et secrets"
-}
-
-variable "name" {
-  type        = string
-  description = "The bucket’s name"
-}
