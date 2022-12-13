3585d114a07c558b56c6bdd6daa435a64824f774
Mathieu Grzybek <mathieu@grzybek.fr>
2022-12-13 19:17:12 +0100

    [operator] add a provisioner using kubectl wait

--- strimzi-operator/main.tf
+++ strimzi-operator/main.tf
@@ -1,4 +1,12 @@
 resource "kubernetes_manifest" "subscription_strimzi_kafka_operator" {
+  provisioner "local-exec" {
+    command = join(" ", [
+      "kubectl wait",
+      "operator/strimzi-kafka-operator.%{if var.isOpenshift}openshift-%{endif}operators",
+      "--for=condition=Ready --timeout=600s"
+    ])
+  }
+
   manifest = yamldecode(templatefile(
     "${path.module}/operator.tmpl.yml",
     {
